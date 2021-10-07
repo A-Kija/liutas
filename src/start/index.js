2b226dff85af03699a31bb74ba5ad076fe9b5e15
A-Kija <42836152+A-Kija@users.noreply.github.com>
2021-10-07 15:01:53 +0300

    start 5

--- src/start/index.js
+++ src/start/index.js
@@ -1,10 +1,15 @@
-const Div = (t) => {
-    const div = document.createElement('div');
-    const text = document.createTextNode(t);
-    div.appendChild(text);
-    const root = document.querySelector('#root');
-    root.appendChild(div);
+const Elem = (t, el, className) => {
+    const div = document.createElement(el); // sukuria tuscia div arba p arba a
+    const text = document.createTextNode(t); // sukuria teksta
+    const root = document.querySelector('#root'); // surandam vieta
+    root.appendChild(div); // pridedam i matoma narsykleje vieta
+    div.appendChild(text); // pridedam teksta i elementa
+    div.classList.add(className);
 }
 
 
 const data = ['Labas, Bebrai', 'Nu sveikutis, Barsukai', 'Kur aini?', 'Namo ainu'];
+
+// const a = salyga ? taip : ne;
+
+data.forEach((s, nr) => Elem(s, 'h1', nr % 2 === 0 ? 'pink' : 'goth'));
