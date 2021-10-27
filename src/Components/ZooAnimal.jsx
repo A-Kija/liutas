00c5a37e02440508db07c0fdbe43e6a1d6866d61
A-Kija <arvydas.kijakauskas@gmail.com>
2021-10-27 11:59:01 +0300

    render names

---
+++ src/Components/ZooAnimal.jsx
@@ -0,0 +1,11 @@
+
+function ZooAnimal({animal}) {
+
+    return (
+        <div className="zoo__list__animal">
+            <div>{animal.name}</div>
+        </div>
+    )
+}
+
+export default ZooAnimal;
