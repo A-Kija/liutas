32a1cb428c909cab86f1f448c0cabe9eac921c25
A-Kija <arvydas.kijakauskas@gmail.com>
2021-11-10 13:47:25 +0200

    Contex

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,41 @@
-import {
-    BrowserRouter as Router,
-    Link,
-    Routes,
-    Route
-} from "react-router-dom";
-import RouterAfrica from "./Components/RouterAfrica";
-import RouterAntarctica from "./Components/RouterAntarctica";
-import RouterAustralia from "./Components/RouterAustralia";
-import RouterBasic from "./Components/RouterBasic";
-import RouterNotFound from "./Components/RouterNotFound";
-import RouterPenguin from "./Components/RouterPenguin";
-import RouterPolarBear from "./Components/RouterPolarBear";
-import RouterSeal from "./Components/RouterSeal";
+import { createContext, useState } from "react";
+import GranPa from "./Components/Context/GranPa";
+export const Msg1 = createContext(''); // <=== gaunam komponenta, ne hookas
+export const Msg2 = createContext(''); // <=== gaunam komponenta, ne hookas
 
 
 function App() {
 
+    const [message1, setMesage1] = useState('No Message 1');
+    const [message2, setMesage2] = useState('No Message 2');
+    const [message1ToChild, setMessage1ToChild] = useState('')
+    const [message2ToChild, setMessage2ToChild] = useState('')
+    
+    const messageControl1 = e => {
+        setMesage1(e.target.value);
+    }
+    const messageControl2 = e => {
+        setMesage2(e.target.value);
+    }
+
+    const sender = () => {
+        setMessage1ToChild(message1);
+        setMessage2ToChild(message2);
+    }
 
     return (
-        <Router>
-            <h1>Hello there!</h1>
-            <ul className="hello-menu">
-                <li><Link to="/">Basic</Link></li>
-                <li><Link to="/africa/77">Africa 77</Link></li>
-                <li><Link to="/africa/14">Africa 14</Link></li>
-                <li><Link to="/africa/24">Africa 24</Link></li>
-                <li><Link to="/australia/15/kangaroo">Australia 15 kangaroo</Link></li>
-                <li><Link to="/australia/354/platypus">Australia 354 platypus</Link></li>
-                <li><Link to="/antarctica">Antarctica</Link></li>
-            </ul>
-            <Routes>
-                <Route index element={<RouterBasic/>}></Route>
-                <Route path="/africa/:times" element={<RouterAfrica/>}></Route>
-                <Route path="/australia/:times/:from" element={<RouterAustralia/>}></Route>
-                <Route path="/antarctica" element={<RouterAntarctica/>}>
-                    <Route path="penguin" element={<RouterPenguin/>}></Route>
-                    <Route path="polar-bear" element={<RouterPolarBear/>}></Route>
-                    <Route path="seal" element={<RouterSeal/>}></Route>
-                </Route>
-                <Route path="*" element={<RouterNotFound/>} />
-            </Routes>
-        </Router>
+        <div className="context">
+            <h1>APP</h1>
+            <input type="text" onChange={messageControl1} value={message1}></input>
+            <input type="text" onChange={messageControl2} value={message2}></input>
+            <button onClick={sender}>APP message</button>
+            <Msg1.Provider value={message1ToChild}>
+            <Msg2.Provider value={message2ToChild}>
+            <GranPa/>
+            </Msg2.Provider>
+            </Msg1.Provider>
+        </div>
+
     )
 }
 
