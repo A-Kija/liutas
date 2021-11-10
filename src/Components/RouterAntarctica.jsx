fa7711582e9635f0a255e4618b63f327762e9186
A-Kija <arvydas.kijakauskas@gmail.com>
2021-11-10 11:43:34 +0200

    Arctic animals

--- src/Components/RouterAntarctica.jsx
+++ src/Components/RouterAntarctica.jsx
@@ -1,6 +1,15 @@
+import { Link } from "react-router-dom";
+
 function RouterAntarctica () {
     return (
-        <h2>Antarctica Hello</h2>
+        <div className="antarctica">
+        <h2>Antarctica</h2>
+        <ul className="antarctica__menu">
+            <li><Link to="penguin">Penguin</Link></li>
+            <li><Link to="polar-bear">Polar Bear</Link></li>
+            <li><Link to="seal">Seal</Link></li>
+        </ul>
+        </div>
     )
 }
 export default RouterAntarctica;
