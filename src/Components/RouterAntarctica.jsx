48f8695372d3bd544dbdea93ccae779c3ac91ae2
A-Kija <arvydas.kijakauskas@gmail.com>
2021-11-10 12:22:48 +0200

    Active Link

--- src/Components/RouterAntarctica.jsx
+++ src/Components/RouterAntarctica.jsx
@@ -1,13 +1,14 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import RouterCustomLink from "./RouterCustomLink";
 
 function RouterAntarctica () {
     return (
         <div className="antarctica">
         <h2>Antarctica</h2>
         <ul className="antarctica__menu">
-            <li><Link to="penguin">Penguin</Link></li>
-            <li><Link to="polar-bear">Polar Bear</Link></li>
-            <li><Link to="seal">Seal</Link></li>
+            <li><RouterCustomLink to="penguin" children="Penguin"/></li>
+            <li><RouterCustomLink to="polar-bear" children="Polar Bear"/></li>
+            <li><RouterCustomLink to="seal" children="Seal"/></li>
         </ul>
         <Outlet/>
         </div>
