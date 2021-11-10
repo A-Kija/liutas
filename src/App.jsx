55b5141513d3dfedb4a88e5147c3c52f28de89c1
A-Kija <arvydas.kijakauskas@gmail.com>
2021-11-10 10:46:45 +0200

    Router Params

--- src/App.jsx
+++ src/App.jsx
@@ -18,14 +18,17 @@ function App() {
             <h1>Hello there!</h1>
             <ul className="hello-menu">
                 <li><Link to="/">Basic</Link></li>
-                <li><Link to="/africa">Africa</Link></li>
-                <li><Link to="/australia">Australia</Link></li>
+                <li><Link to="/africa/77">Africa 77</Link></li>
+                <li><Link to="/africa/14">Africa 14</Link></li>
+                <li><Link to="/africa/24">Africa 24</Link></li>
+                <li><Link to="/australia/15/kangaroo">Australia 15 kangaroo</Link></li>
+                <li><Link to="/australia/354/platypus">Australia 354 platypus</Link></li>
                 <li><Link to="/antarctica">Antarctica</Link></li>
             </ul>
             <Routes>
-                <Route path="/" element={<RouterBasic/>}></Route>
-                <Route path="/africa" element={<RouterAfrica/>}></Route>
-                <Route path="/australia" element={<RouterAustralia/>}></Route>
+                <Route path="" element={<RouterBasic/>}></Route>
+                <Route path="/africa/:times" element={<RouterAfrica/>}></Route>
+                <Route path="/australia/:times/:from" element={<RouterAustralia/>}></Route>
                 <Route path="/antarctica" element={<RouterAntarctica/>}></Route>
             </Routes>
         </Router>
